f3a3936a555c407c719abfcca436de1c807e539f
D3NMOH <d3nmoh@gmail.com>
2024-03-08 13:18:10 +0100

    vercel

--- src/components/TopBar/index.jsx
+++ src/components/TopBar/index.jsx
@@ -5,28 +5,26 @@ import { Link, NavLink } from "react-router-dom";
 export default function TopBar({ handleOpenCart }) {
   return (
     <div className={styles.TopBar}>
-      <div className={styles.topBarButton}>
-        <i className="fa-solid fa-circle-user"></i>
-      </div>
       <nav className={styles.navigation}>
-        <img src={Logo} alt="Logo" className={styles.logo} />
-        <NavLink
-          to={`/HomePage`}
-          className={styles.topBarButton}
-          style={{ textDecoration: "none" }}
-        >
-          Home
+        <NavLink to={`/HomePage`} className={styles.topBarButton}>
+          <i className="fa-solid fa-house-chimney"></i>
+          <p className={styles.navTitle}>Home</p>
         </NavLink>
-        <NavLink
-          to={`/Products`}
-          className={styles.topBarButton}
-          style={{ textDecoration: "none" }}
-        >
-          Products
+        <NavLink to={`/Products`} className={styles.topBarButton}>
+          <i className="fa-solid fa-list"></i>
+          <p className={styles.navTitle}>Products</p>
         </NavLink>
       </nav>
-      <div onClick={handleOpenCart} className={styles.topBarButton}>
-        <i className="fa-solid fa-cart-shopping"></i>
+      <img src={Logo} alt="Logo" className={styles.logo} />
+      <div className={styles.userblock}>
+        <div className={styles.topBarButton}>
+          <i className="fa-solid fa-circle-user"></i>
+          <p className={styles.navTitle}>User</p>
+        </div>
+        <div onClick={handleOpenCart} className={styles.topBarButton}>
+          <i className="fa-solid fa-cart-shopping"></i>
+          <p className={styles.navTitle}>Shopping cart</p>
+        </div>
       </div>
     </div>
   );
